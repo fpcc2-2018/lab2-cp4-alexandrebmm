6b3c585e082fbf6e7ba536ff4aaf0074e550c409
Alexandre Medeiros <skaleworld@gmail.com>
2018-05-01 23:55:10 -0300

    Conserto da diferença de tempo (tratamento em segundos)

--- code/import-events_to_searches.R
+++ code/import-events_to_searches.R
@@ -7,7 +7,7 @@ message("Lendo dados brutos de eventos")
 
 events_original = read_csv("https://github.com/wikimedia-research/Discovery-Hiring-Analyst-2016/raw/master/events_log.csv.gz")
 
-#events_original = events_original %>% slice(1:1000) # Útil para testar código em dados pequenos. Comente na hora de processá-los para valer.
+#events_original = events_original %>% slice(1:5000) # Útil para testar código em dados pequenos. Comente na hora de processá-los para valer.
 
 message("Transformando em dados por busca")
 
@@ -22,7 +22,7 @@ searches = events %>%
     summarise(
         session_start_timestamp = first(timestamp),
         session_start_date = ymd_hms(first(timestamp)),
-        session_length = max(timestamp) - min(timestamp),
+        session_end_date = ymd_hms(last(timestamp)),
         group = first(group), # eventos de uma mesma sessão são de um mesmo grupo
         results = max(n_results, na.rm = TRUE), # se não houver busca, retorna -Inf
         num_clicks = sum(action == "visitPage"), 
